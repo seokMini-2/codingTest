09cd5b3ccce1fff2718df7a38961a7d373ee21dd
seokMini-2 <lsm927782@gmail.com>
2024-11-04 00:45:23 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[30];
+
+        for (int i = 0; i < 28; i++) {
+            int com = scanner.nextInt();
+            arr[com-1]=1;
+        }
+
+        for (int i = 0; i < 30; i++) {
+            if (arr[i] == 0) {
+                System.out.println(i+1);
+            }
+        }
+
+    }
+}
