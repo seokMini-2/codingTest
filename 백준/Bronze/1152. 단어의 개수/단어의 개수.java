cee17209526f11eea96ebd916fe771b6a5b985c0
seokMini-2 <lsm927782@gmail.com>
2024-11-18 17:37:15 +0900

    [Bronze II] Title: 단어의 개수, Time: 236 ms, Memory: 20016 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer answer = new StringTokenizer(str);
+        System.out.println(answer.countTokens());
+
+
+    }
+}
