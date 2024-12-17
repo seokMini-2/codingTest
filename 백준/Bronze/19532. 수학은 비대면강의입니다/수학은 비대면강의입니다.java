e2aef866c8508f8d2d38e6fa6d1fb1edf9628cdc
seokMini-2 <lsm927782@gmail.com>
2024-12-17 16:53:52 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 132 ms, Memory: 16028 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -13,104 +13,13 @@ public class Main {
         int d = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
         int f = Integer.parseInt(st.nextToken());
-        int x = 0, y = 0;
-        if (b > 0 && e > 0) {
-            for (int i = 1; i <= e; i++) {
-                for (int j = 1; j <= b; j++) {
-                    if (b * i == j * e) {
-                        b = b * i;
-                        a *= i;
-                        c *= i;
-                        e = b;
-                        d *= j;
-                        f *= j;
-                        break;
-                    }
-                }
-                if (e == b) {
-                    break;
-                }
-            }
-            x = (c - f) / (a - d);
-            y = (c - a * x) / b;
-            System.out.println(x + " " + y);
-        } else if (b < 0 && e < 0) {
-            for (int i = 1; i <= e*(-1); i++) {
-                for (int j = 1; j <= b*(-1); j++) {
-                    if (b * i == j * e) {
-                        b = b * i;
-                        a *= i;
-                        c *= i;
-                        e = b;
-                        d *= j;
-                        f *= j;
-                        break;
-                    }
-                }
-                if (e == b) {
-                    break;
-                }
-            }
-            x = (c - f) / (a - d);
-            y = (c - a * x) / b;
-            System.out.println(x + " " + y);
+        XY(a, b, c, d, e, f);
 
-        } else if (b < 0 || e < 0) {
+    }
 
-            if (b < 0) {
-                for (int i = 1; i <= e; i++) {
-                    for (int j = 1; j <= b * (-1); j++) {
-                        if (b * i * (-1) == j * e) {
-                            b = b * i;
-                            a *= i;
-                            c *= i;
-                            e *= j;
-                            d *= j;
-                            f *= j;
-                            break;
-                        }
-                    }
-                    if (e * (-1) == b) {
-                        break;
-                    }
-                }
-                x = (c + f) / (a + d);
-                y = (c - a * x) / b;
-                System.out.println(x + " " + y);
-
-            } else {
-                for (int i = 1; i <= e * (-1); i++) {
-                    for (int j = 1; j <= b; j++) {
-                        if (b * i == j * e * (-1)) {
-                            b = b * i;
-                            a *= i;
-                            c *= i;
-                            e *= j;
-                            d *= j;
-                            f *= j;
-                            break;
-                        }
-                    }
-                    if (e * (-1) == b) {
-                        break;
-                    }
-                }
-                x = (c + f) / (a + d);
-                y = (c - a * x) / b;
-                System.out.println(x + " " + y);
-
-            }
-
-        } else {
-            if (b == 0) {
-                x = c / a;
-                y = (f - d * x) / e;
-            } else {
-                x = f / d;
-                y = (c - a * x) / b;
-            }
-            System.out.println(x + " " + y);
-        }
-
+    private static void XY(int a, int b, int c, int d, int e, int f) {
+        int x = (c * e - b * f) / (a * e - b * d);
+        int y = (c * d - a * f) / (b * d - a * e);
+        System.out.println(x + " " + y);
     }
 }
